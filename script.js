bd8c7af1523330a0a7405d81b6e9d7ffd8c2df78
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-10-13 18:14:07 -0300

    just need some polish now

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-const getData = () => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const getData = async () => {
+	try {
+		const response = await fetch(
+			"https://private-3923c4-santandercoders809.apiary-mock.com/stations",
+		);
+		const stations = await response.json();
+		return stations;
+	} catch (error) {
+		console.log(`Error ${error.name}: `, error.message);
+	}
+};
+
+// {
+// 	"_estacaoId": "152",
+// 	"_nome": "Ana Rosa",
+// 	"_ordem": "01",
+// 	"_linhaId": "1",
+// 	"_linha": "1-Azul",
+// 	"_tipoId": "1",
+// 	"_tipo": "Metrô"
+// },
+
+const dataReducer = (data) => {
+	const result = data.reduce((acumulator, station) => {
+		let metroLine = station._linha;
+		Object.keys(acumulator).includes(metroLine)
+			? (acumulator[metroLine] = [...acumulator[metroLine], station])
+			: (acumulator[metroLine] = [station]);
+
+		return acumulator;
+	}, {});
+
+	console.log(result);
+};
+
+// (pokemons) => {
+// 	const pokemonLI = pokemons.reduce((acumulador, pokemon) => {
+// 		let typeName = pokemon.types[0].type.name;
+// 		Object.keys(acumulador).includes(typeName) ?
+// 			acumulador[typeName] = [...acumulador[typeName], pokemon.name] : acumulador[typeName] = [pokemon.name];
+
+// 		return acumulador;
+// 	}, {});
+
+const orderData = async () => {
+	const stationsData = await getData();
+	if (!stationsData) {
+		console.log("erro na aquisição dos dados");
+		return;
+	}
+	console.log(stationsData);
+	dataReducer(stationsData.estacoes.estacao);
+};
+
+orderData();
